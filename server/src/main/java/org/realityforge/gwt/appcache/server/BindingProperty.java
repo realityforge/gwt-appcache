e0b06581ae73bb82bc73d7d28fca494766e3d953
Peter Donald <peter@realityforge.org>
2013-12-09 09:09:18 +1100

    Add nullability annotations

--- server/src/main/java/org/realityforge/gwt/appcache/server/BindingProperty.java
+++ server/src/main/java/org/realityforge/gwt/appcache/server/BindingProperty.java
@@ -1,21 +1,25 @@
 package org.realityforge.gwt.appcache.server;
 
+import javax.annotation.Nonnull;
+
 public final class BindingProperty
 {
   private final String _name;
   private final String _value;
 
-  public BindingProperty( String name, String value )
+  public BindingProperty( @Nonnull final String name, @Nonnull final String value )
   {
     _name = name;
     _value = value;
   }
 
+  @Nonnull
   public String getName()
   {
     return _name;
   }
 
+  @Nonnull
   public String getValue()
   {
     return _value;
