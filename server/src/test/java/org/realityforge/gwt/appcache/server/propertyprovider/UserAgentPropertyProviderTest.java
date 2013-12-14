28df73b944f8ef217ed6db18353072e7f8288413
Peter Donald <peter@realityforge.org>
2013-12-15 07:38:42 +1100

    Add more user agents to test

--- server/src/test/java/org/realityforge/gwt/appcache/server/propertyprovider/UserAgentPropertyProviderTest.java
+++ server/src/test/java/org/realityforge/gwt/appcache/server/propertyprovider/UserAgentPropertyProviderTest.java
@@ -12,16 +12,19 @@ public class UserAgentPropertyProviderTest
   public Object[][] getAgentMapping()
   {
     return new Object[][] {
-      { "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.57 Safari/537.36",
-        "safari" },
-      { " Mozilla/5.0 (iPad; U; CPU OS 3_2_1 like Mac OS X; en-us) AppleWebKit/531.21.10 (KHTML, like Gecko) Mobile/7B405", "safari" },
-      { "Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; Trident/6.0)", "ie10" },
-      { "Mozilla/5.0 (Windows; U; MSIE 9.0; WIndows NT 9.0; en-US))", "ie9" },
-      { "Mozilla/5.0 (compatible; MSIE 8.0; Windows NT 6.1; Trident/4.0; GTB7.4; InfoPath.2; SV1; .NET CLR 3.3.69573; WOW64; en-US)", "ie8" },
+      { UserAgents.Desktop.CHROME, "safari" },
+      { UserAgents.Mobile.IPAD_IOS5, "safari" },
+      { UserAgents.Mobile.IPAD, "safari" },
+      { UserAgents.Mobile.IPHONE_IOS5, "safari" },
+      { UserAgents.Mobile.ANDROID_PHONE_2x, "safari" },
+      { UserAgents.Mobile.ANDROID_TABLET, "safari" },
+      { UserAgents.Desktop.IE10, "ie10" },
+      { UserAgents.Desktop.IE9, "ie9" },
+      { UserAgents.Desktop.IE8, "ie8" },
       //Assume that anyone asking for IE7 is doing it due to compatibility mode. Blargh!
-      { "Mozilla/5.0 (compatible; MSIE 7.0; Windows NT 5.0; Trident/4.0; FBSMTWB; .NET CLR 2.0.34861; .NET CLR 3.0.3746.3218; .NET CLR 3.5.33652; msn OptimizedIE8;ENUS)", "ie8" },
-      { "Opera/9.80 (Windows NT 6.1; U; ko) Presto/2.7.62 Version/11.00", "opera" },
-      { "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:18.0) Gecko/20100101 Firefox/18.0", "gecko1_8" },
+      { UserAgents.Desktop.IE7, "ie8" },
+      { UserAgents.Desktop.OPERA, "opera" },
+      { UserAgents.Desktop.FIREFOX, "gecko1_8" },
     };
   }
 
---
+++ server/src/test/java/org/realityforge/gwt/appcache/server/propertyprovider/UserAgents.java
@@ -0,0 +1,34 @@
+package org.realityforge.gwt.appcache.server.propertyprovider;
+
+interface UserAgents
+{
+  interface Desktop
+  {
+    String FIREFOX = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:18.0) Gecko/20100101 Firefox/18.0";
+    String OPERA = "Opera/9.80 (Windows NT 6.1; U; ko) Presto/2.7.62 Version/11.00";
+    String CHROME =
+      "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.57 Safari/537.36";
+    String IE10 = "Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; Trident/6.0)";
+    String IE9 = "Mozilla/5.0 (Windows; U; MSIE 9.0; WIndows NT 9.0; en-US))";
+    String IE8 =
+      "Mozilla/5.0 (compatible; MSIE 8.0; Windows NT 6.1; Trident/4.0; GTB7.4; InfoPath.2; SV1; .NET CLR 3.3.69573; WOW64; en-US)";
+    String IE7 =
+      "Mozilla/5.0 (compatible; MSIE 7.0; Windows NT 5.0; Trident/4.0; FBSMTWB; .NET CLR 2.0.34861; .NET CLR 3.0.3746.3218; .NET CLR 3.5.33652; msn OptimizedIE8;ENUS)";
+  }
+
+  interface Mobile
+  {
+    String BLACKBERRY =
+      "Mozilla/5.0 (BlackBerry; U; BlackBerry 9780; de) AppleWebKit/534.1+ (KHTML, like Gecko) Version/6.0.0.359 Mobile Safari/534.1+";
+    String ANDROID_TABLET =
+      "Mozilla/5.0 (Linux; U; Android 3.0; en-us; Xoom Build/HRI39) AppleWebKit/534.13 (KHTML, like Gecko) Version/4.0 Safari/534.13";
+    String ANDROID_PHONE_2x =
+      "Mozilla/5.0 (Linux; U; Android 2.3.6; de-de; Nexus S Build/GRK39F) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1";
+    String IPAD_IOS5 =
+      "Mozilla/5.0 (iPad; CPU OS 5_0 like Mac OS X) AppleWebKit/534.46 (KHTML, like Gecko) Version/5.1 Mobile/9A334 Safari/7534.48.3";
+    String IPAD =
+      "Mozilla/5.0 (iPad; U; CPU OS 3_2_1 like Mac OS X; en-us) AppleWebKit/531.21.10 (KHTML, like Gecko) Mobile/7B405";
+    String IPHONE_IOS5 =
+      "Mozilla/5.0 (iPhone; CPU iPhone OS 5_0 like Mac OS X) AppleWebKit/534.46 (KHTML, like Gecko) Version/5.1 Mobile/9A334 Safari/7534.48.3";
+  }
+}
